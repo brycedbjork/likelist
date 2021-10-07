36be4c9ca10a98dc336b6fdab8c313ff0b5e2eca
Bryce Bjork <brycedbjork@gmail.com>
2021-10-07 19:33:07 -0400

    reduced oauth scopes

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -8,17 +8,9 @@ const auth = (req: NextApiRequest, res: NextApiResponse) => {
     // Scopes to request.
     const OAUTH_SCOPES = [
       "ugc-image-upload",
-      "playlist-read-collaborative",
       "playlist-modify-public",
-      "user-library-modify",
-      "user-top-read",
-      "playlist-read-private",
-      "user-follow-read",
-      "user-read-recently-played",
-      "playlist-modify-private",
       "user-library-read",
       "user-read-email",
-      "user-read-private",
     ];
 
     const authorizeURL = Spotify.createAuthorizeURL(OAUTH_SCOPES, state);
