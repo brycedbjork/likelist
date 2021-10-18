5aee04d59477b8dcb53447bdfd9b40a228d99112
Bryce Bjork <brycedbjork@gmail.com>
2021-10-18 16:16:22 -0400

    lowercase slugs

--- db/chooseSlug.ts
+++ db/chooseSlug.ts
@@ -13,14 +13,15 @@ const chooseSlug = async (data: {
   slug: string;
 }): Promise<void> => {
   const { uid, slug } = data;
+  const lowercaseSlug = slug.toLowerCase();
   const userDocs = await getDocs(
-    query(collection(firestore, `/users`), where("slug", "==", slug))
+    query(collection(firestore, `/users`), where("slug", "==", lowercaseSlug))
   );
   const slugAvailable = userDocs.size === 0;
   if (!slugAvailable) throw new Error("Slug not available");
 
   await updateDoc(doc(firestore, `/users/${uid}`), {
-    slug,
+    slug: lowercaseSlug,
   });
 };
 
--- db/testSlug.ts
+++ db/testSlug.ts
@@ -3,8 +3,9 @@ import { collection, getDocs, query, where } from "@firebase/firestore";
 
 // function that takes slug (eg. "bryce" for bryce.likelist.xyz) and return boolean available or not
 const testSlug = async (slug: string): Promise<boolean> => {
+  const lowercaseSlug = slug.toLowerCase();
   const userDocs = await getDocs(
-    query(collection(firestore, `/users`), where("slug", "==", slug))
+    query(collection(firestore, `/users`), where("slug", "==", lowercaseSlug))
   );
   const slugAvailable = userDocs.size === 0;
   return slugAvailable;
